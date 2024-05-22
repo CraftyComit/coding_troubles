375e682ae3901adf91b7aa8111f87374d5cc717f
Zeke <hollandzeke@gmail.com>
2024-05-21 20:46:34 -0500

    completly resorted code added a restart key along with a quit key

--- learning.py
+++ learning.py
@@ -3,9 +3,6 @@ from pygame.locals import *
 import sys
 import random
 import time
-
- 
-pygame.init()
 vec = pygame.math.Vector2 #2 for two dimensional
 blue = (0,54,92)
 white = (255, 255, 255)
@@ -16,35 +13,6 @@ ACC = 0.5
 FRIC = -0.12
 FPS = 60
 screen = 1
-pressed_keys = pygame.key.get_pressed()
-z= 1
-font = pygame.font.Font('freesansbold.ttf', 22)
-text = font.render('YOU DIED', True, green, blue)
-textRect = text.get_rect()
-ont = pygame.font.Font('freesansbold.ttf', 32)
-ext = font.render('I will now crash', True, green, blue)
-extRect = text.get_rect()
-fon = pygame.font.Font('freesansbold.ttf', 32)
-tex = font.render('From turtle programing studios', True, green, blue)
-textRec = text.get_rect()
-textRec.center = (WIDTH // 5, HEIGHT // 2)
-FramePerSec = pygame.time.Clock()
- 
-displaysurface = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("programming torture")
-if screen == 1:
-    displaysurface.fill((0,54,92))
-    displaysurface.blit(tex, textRec)
-    pygame.display.update()
-    time.sleep(5)
-    #os.waitpid(pressed_keys[K_j])
-#    if pressed_keys[K_j]:
-#        z = 2
-#        time.sleep(9999999999999999999999999999999999999999)
-
-    pygame.init()
-textRect.center = (WIDTH // 2, HEIGHT // 2)
-extRect.center = (WIDTH // 2.5, HEIGHT // 1.75)
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__() 
@@ -82,7 +50,7 @@ class Player(pygame.sprite.Sprite):
              
         self.rect.midbottom = self.pos
  
-    def jump(self): 
+    def jump(self, platforms): 
         hits = pygame.sprite.spritecollide(self, platforms, False)
         if hits and not self.jumping:
            self.jumping = True
@@ -93,7 +61,7 @@ class Player(pygame.sprite.Sprite):
             if self.vel.y < -3:
                 self.vel.y = -3
  
-    def update(self):
+    def update(self, platforms):
         hits = pygame.sprite.spritecollide(self ,platforms, False)
         if self.vel.y > 0:        
             if hits:
@@ -113,8 +81,6 @@ class platform(pygame.sprite.Sprite):
  
     def move(self):
         pass
- 
- 
 def check(platform, groupies):
     if pygame.sprite.spritecollideany(platform,groupies):
         return True
@@ -126,7 +92,7 @@ def check(platform, groupies):
                 return True
         C = False
  
-def plat_gen():
+def plat_gen(platforms, all_sprites):
     while len(platforms) < 6:
         width = random.randrange(50,100)
         p  = platform()      
@@ -140,72 +106,125 @@ def plat_gen():
         platforms.add(p)
         all_sprites.add(p)
  
- 
-        
-PT1 = platform()
-P1 = Player()
- 
-PT1.surf = pygame.Surface((WIDTH, 20))
-PT1.surf.fill((0,0,0))
-PT1.rect = PT1.surf.get_rect(center = (WIDTH/2, HEIGHT - 10))
- 
-all_sprites = pygame.sprite.Group()
-all_sprites.add(PT1)
-all_sprites.add(P1)
- 
-platforms = pygame.sprite.Group()
-platforms.add(PT1)
- 
-for x in range(random.randint(4,5)):
-    C = True
-    pl = platform()
-    while C:
+def main():
+    pygame.init()
+    vec = pygame.math.Vector2 #2 for two dimensional
+    blue = (0,54,92)
+    white = (255, 255, 255)
+    green = (0, 255, 0)
+    HEIGHT = 450
+    WIDTH = 400
+    ACC = 0.5
+    FRIC = -0.12
+    FPS = 60
+    screen = 1
+    pressed_keys = pygame.key.get_pressed()
+    font = pygame.font.Font('freesansbold.ttf', 22)
+    text = font.render('YOU DIED', True, green, blue)
+    textRect = text.get_rect()
+    ont = pygame.font.Font('freesansbold.ttf', 32)
+    ext = font.render('press j to restart', True, green, blue)
+    extRect = text.get_rect()
+    fon = pygame.font.Font('freesansbold.ttf', 32)
+    tex = font.render('From LB programing studios', True, green, blue)
+    tet = font.render('press space to play', True, green, blue)
+    textRet = text.get_rect()
+    textRet.center = (WIDTH // 2.7, HEIGHT // 2)
+    textRec = text.get_rect()
+    textRec.center = (WIDTH // 4.5, HEIGHT // 2)
+    FramePerSec = pygame.time.Clock()
+    #button_surface = pygame.surface((100, 30))
+    # button_text = font.render("click me", True, (0,0,0))
+    displaysurface = pygame.display.set_mode((WIDTH, HEIGHT))
+    pygame.display.set_caption("programming torture")
+    if screen == 1:
+        displaysurface.fill((0,54,92))
+        displaysurface.blit(tex, textRec)
+        pygame.display.update()
+        time.sleep(2)
+        screen = 4
+    if screen == 4:
+        displaysurface.fill((0,54,92))
+        displaysurface.blit(tet, textRet)
+        pygame.display.update()
+        time.sleep(2)
+
+
+        pygame.init()
+    textRect.center = (WIDTH // 2, HEIGHT // 2)
+    extRect.center = (WIDTH // 2.5, HEIGHT // 1.75)
+
+    PT1 = platform()
+    P1 = Player()
+    
+    PT1.surf = pygame.Surface((WIDTH, 20))
+    PT1.surf.fill((0,0,0))
+    PT1.rect = PT1.surf.get_rect(center = (WIDTH/2, HEIGHT - 10))
+    
+    all_sprites = pygame.sprite.Group()
+    all_sprites.add(PT1)
+    all_sprites.add(P1)
+    
+    platforms = pygame.sprite.Group()
+    platforms.add(PT1)
+    
+    for x in range(random.randint(4,5)):
+        C = True
         pl = platform()
-        C = check(pl, platforms)
-    platforms.add(pl)
-    all_sprites.add(pl)
+        while C:
+            pl = platform()
+            C = check(pl, platforms)
+        platforms.add(pl)
+        all_sprites.add(pl)
  
- 
-while True:
-    P1.update()
-    for event in pygame.event.get():
-        if event.type == QUIT:
-            pygame.quit()
-            sys.exit()
-        if event.type == pygame.KEYDOWN:    
-            if event.key == pygame.K_w:
-                P1.jump()
-        if event.type == pygame.KEYUP:    
-            if event.key == pygame.K_w:
-                P1.cancel_jump()  
- 
-    if P1.rect.top <= HEIGHT / 3:
-        P1.pos.y += abs(P1.vel.y)
-        for plat in platforms:
-            plat.rect.y += abs(P1.vel.y)
-            if plat.rect.top >= HEIGHT:
-                plat.kill()
-    if P1.rect.top > HEIGHT:
-        for entity in all_sprites:
-            entity.kill()
-            time.sleep(1)
+    while True:
+        P1.update(platforms)
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.KEYDOWN:    
+                if event.key == pygame.K_w:
+                    P1.jump(platforms)
+            if event.type == pygame.KEYUP:    
+                if event.key == pygame.K_w:
+                    P1.cancel_jump()  
+    
+        if P1.rect.top <= HEIGHT / 3:
+            P1.pos.y += abs(P1.vel.y)
+            for plat in platforms:
+                plat.rect.y += abs(P1.vel.y)
+                if plat.rect.top >= HEIGHT:
+                    plat.kill()
+        if P1.rect.top > HEIGHT:
+            for entity in all_sprites:
+                entity.kill()
             displaysurface.fill((0,54,92))
             pygame.display.update()
-            time.sleep(1)
             screen = 3
             displaysurface.blit(text, textRect)
             displaysurface.blit(ext, extRect)
             pygame.display.update()
-            time.sleep(5)
-            pygame.quit()
-            sys.exit()
- 
-    plat_gen()
-    displaysurface.fill((0,64,92))
-     
-    for entity in all_sprites:
-        displaysurface.blit(entity.surf, entity.rect)
-        entity.move()
- 
-    pygame.display.update()
-    FramePerSec.tick(FPS)
+            time.sleep(0.25)
+            while True:
+                for event in pygame.event.get():
+                    if event.type == pygame.KEYDOWN: 
+                        if event.key == pygame.K_j:
+                            main()
+                        elif event.key == pygame.K_q:        
+                            pygame.quit()
+                            sys.exit()
+                
+
+        plat_gen(platforms,all_sprites)
+        displaysurface.fill((0,64,92))
+        
+        for entity in all_sprites:
+            displaysurface.blit(entity.surf, entity.rect)
+            entity.move()
+    
+        pygame.display.update()
+        FramePerSec.tick(FPS)
+
+if __name__ == "__main__":
+    main()
